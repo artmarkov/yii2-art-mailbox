0e072989e33f8654baf27b3dcbf8b5250bc63cb6
artmarkov <artmarkov@mail.ru>
2019-10-21 23:58:25 +0300

    refactor Job

---
+++ jobs/MessageNewEmailJob.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace artsoft\mailbox\jobs;
+use artsoft\mailbox\models\MailboxInbox;
+use Yii;
+//use yii\helpers\Html;
+//use yii\helpers\Url;
+
+/**
+ * Class MessageNewEmailstJob.
+ */
+class MessageNewEmailJob extends \yii\base\BaseObject implements \yii\queue\JobInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function execute($queue)
+    {
+        foreach ($this->getQtyNewMail() as $model) {
+             $this->sendEmail($model);
+        }
+    }
+    
+    /**
+     * 
+     * @return type
+     */
+    public static function getQtyNewMail() {
+        return MailboxInbox::find()
+                        ->joinWith(['receiver'])
+                        ->select(['receiver_id', 'COUNT(*) AS qty'])
+                        ->groupBy('receiver_id')
+                        ->readNew()
+                        ->andWhere(['status' => \artsoft\models\User::STATUS_ACTIVE])
+                        ->asArray()
+                        ->all();
+    }
+    /**
+     * 
+     * @param type $model
+     * @return type
+     */
+    public static function sendEmail($model)
+    {
+//        $link = Url::to(['/admin/default/mailbox'], true);
+        
+        $textBody = 'Здравствуйте, ' . strip_tags($model['receiver']['username']) . PHP_EOL;
+        $textBody .= 'По состоянию на ' . date('d.n.Y H:i', time()) . ' в Ваш почтовый ящик поступило ' . strip_tags($model['qty']) . 'новых писем.' . PHP_EOL . PHP_EOL;
+//        $textBody .=  Html::a(Html::encode($link), $link);
+
+        $htmlBody = '<p><b>Здравствуйте</b>, ' . strip_tags($model['receiver']['username']) . '</p>';
+        $htmlBody .= '<p>По состоянию на ' . date('d.n.Y H:i', time()) . ' в Ваш почтовый ящик поступило ' . strip_tags($model['qty']) . ' новых писем.</p>';        
+//        $htmlBody .= '<p>' . Html::a(Html::encode($link), $link) . '</p>';
+        
+        return Yii::$app->mailer->compose()
+            ->setFrom(Yii::$app->params['adminEmail'])
+            ->setTo($model['receiver']['email'])
+            ->setSubject('Сообщение с сайта ' . Yii::$app->name)
+            ->setTextBody($textBody)
+            ->setHtmlBody($htmlBody)
+            ->send();
+    }
+}
