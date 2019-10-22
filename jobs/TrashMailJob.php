64b868748f18095e80f8745ca01fc94c0ecc9206
artmarkov <artmarkov@mail.ru>
2019-10-22 23:02:00 +0300

    add Job

---
+++ jobs/TrashMailJob.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace artsoft\mailbox\jobs;
+
+/**
+ * Class MessageNewEmailstJob.
+ */
+class TrashMailJob extends \yii\base\BaseObject implements \yii\queue\JobInterface
+{
+    public $modelClass = 'artsoft\mailbox\models\Mailbox';
+    public $modelViaClass = 'artsoft\mailbox\models\MailboxInbox';
+    
+    /**
+     * @inheritdoc
+     */
+    public function execute($queue)
+    {
+        $id = $this->modelClass::getTrashMail();
+        if (!empty($id)) {
+
+            $where = ['id' => $id, 'status_del' => $this->modelClass::STATUS_DEL_TRASH];
+            $this->modelClass::updateAll(['status_del' => $this->modelClass::STATUS_DEL_DELETE, 'deleted_at' => time()], $where);
+
+            $whereVia = ['mailbox_id' => $id, 'status_del' => $this->modelClass::STATUS_DEL_TRASH];
+            $this->modelViaClass::updateAll(['status_del' => $this->modelClass::STATUS_DEL_DELETE, 'deleted_at' => time()], $whereVia);
+        }
+    }   
+}
--- migrations/m191022_171748_add_mailbox_job_to_queue_schedule.php
+++ migrations/m191022_171748_add_mailbox_job_to_queue_schedule.php
@@ -8,11 +8,13 @@ class m191022_171748_add_mailbox_job_to_queue_schedule extends Migration
     {
        $this->insert('{{%queue_schedule}}', ['class' => 'artsoft\mailbox\jobs\ClianDeletedMailJob', 'title' => 'Уничтожение удаленных писем', 'content' => 'Удаляет все письма физически из корзины.', 'cron_expression' => '0 0 */15 * *', 'priority' => 1024 ,'created_at' => time(), 'updated_at' => time(), 'created_by' => 1, 'updated_by' => 1]);
        $this->insert('{{%queue_schedule}}', ['class' => 'artsoft\mailbox\jobs\MessageNewEmailJob', 'title' => 'Оповещение пользователей о новых сообщениях', 'content' => '', 'cron_expression' => '0 8 * * *', 'priority' => 1024 ,'created_at' => time(), 'updated_at' => time(), 'created_by' => 1, 'updated_by' => 1]);
+       $this->insert('{{%queue_schedule}}', ['class' => 'artsoft\mailbox\jobs\TrashMailJob', 'title' => 'Очистка всех корзин', 'content' => 'Очищает корзины всех пользователей', 'cron_expression' => '0 0 1 */6 *', 'priority' => 1024 ,'created_at' => time(), 'updated_at' => time(), 'created_by' => 1, 'updated_by' => 1]);
     }
 
     public function down()
     {
         $this->delete('{{%queue_schedule}}', ['class' => 'artsoft\mailbox\jobs\ClianDeletedMailJob']);
         $this->delete('{{%queue_schedule}}', ['class' => 'artsoft\mailbox\jobs\MessageNewEmailJob']);
+        $this->delete('{{%queue_schedule}}', ['class' => 'artsoft\mailbox\jobs\TrashMailJob']);
     }
 }
